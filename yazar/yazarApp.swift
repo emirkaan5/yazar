e4728115a44dd7977c8b9f895e901b0b1353a1da
is-ege <9782679+is-ege@users.noreply.github.com>
2026-08-29 21:01:21 +0300

    Initial Commit

---
+++ yazar/yazarApp.swift
@@ -0,0 +1,17 @@
+//
+//  yazarApp.swift
+//  yazar
+//
+//  Created by Ege on 29.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct yazarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
